913fc8eee7d6febfe9f5d3bb9e1911271e59e0bc
ookkdai <78086517+ookkdai@users.noreply.github.com>
2021-05-26 18:17:35 +0900

    詳細機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
   def new
-  @posts = Post.new
+  @post = Post.new
   end
-
+  
   def create
     post = Post.create!(post_params)
     redirect_to post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
   private
 
   def post_params
