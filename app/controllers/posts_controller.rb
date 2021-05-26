07c1bf80d90e7b8dcf09a2612eaa206fc618675d
ookkdai <78086517+ookkdai@users.noreply.github.com>
2021-05-26 18:23:01 +0900

    編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,11 +14,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update!(post_params)
+    redirect_to post
+  end
+
   def destroy
     post = Post.find(params[:id])
     post.destroy!
     redirect_to root_path
   end
+
   private
 
   def post_params
