8361a10f970bfd383d05282e6b3f1324bcde60c8
ookkdai <78086517+ookkdai@users.noreply.github.com>
2021-05-26 17:56:25 +0900

    新規投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  def new
+  @posts = Post.new
+  end
+
+  def create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
